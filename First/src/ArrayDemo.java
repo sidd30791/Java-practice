c387c35961e161a8a1ad5ad56ceab41609a55518
siddharth <siddharth@localhost>
2021-01-15 13:19:11 -0700

    Hi

--- First/src/ArrayDemo.java
+++ First/src/ArrayDemo.java
@@ -5,7 +5,7 @@ public class ArrayDemo {
 //		// TODO Auto-generated constructor stub
 //	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {  
 		// TODO Auto-generated method stub
 		 int ary[] = new int[5];
 		 
